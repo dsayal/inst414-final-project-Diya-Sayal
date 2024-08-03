004b577a31728157ed69c2c0d6a32c524cec3a63
dsayal <157721088+dsayal@users.noreply.github.com>
2024-08-03 19:15:15 -0400

    fixing directories

---
+++ main.py
@@ -0,0 +1,25 @@
+from etl.extract import extract_data
+from etl.transform import load_extracted_data, clean_data
+from analysis.model import train_model
+from vis import create_visualizations
+
+def main():
+    # Extract
+    file_path = 'data/raw/Cyber Security Breaches.csv'
+    data = extract_data(file_path)
+    data.to_csv('data/extracted/cyber_security_breaches.csv', index=False)
+    
+    # Transform and load
+    data = load_extracted_data('data/extracted/cyber_security_breaches.csv')
+    cleaned_data = clean_data(data)
+    cleaned_data.to_csv('data/processed/cleaned_cyber_security_breaches.csv', index=False)
+    
+    # Analyze
+    model, accuracy = train_model(cleaned_data)
+    print(f'Model Accuracy: {accuracy}')
+    
+    # Visualize
+    create_visualizations(cleaned_data)
+
+if __name__ == '__main__':
+    main()
